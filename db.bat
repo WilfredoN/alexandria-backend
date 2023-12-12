bcda715356c765b19c3178171e88248c12a1ea02
Nikita <thekillermaps@gmail.com>
2023-12-12 16:00:43 +0200

    delete unnecessary file

--- db.bat
+++
@@ -1,1 +0,0 @@
-docker run --name db -e POSTGRES_DB=alexandria -e POSTGRES_USER=alexandria_login -e POSTGRES_PASSWORD=rotpasdbalexandria -p 5432:5432 -v pgdata:/var/lib/postgresql/data -d postgres:latest
